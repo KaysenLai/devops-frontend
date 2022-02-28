6ed84d17e06c6d12707f8a579e8e20773c9af52c
Chaokai Lai <chaokai.lai@gmail.com>
2022-02-28 21:25:52 +0800

    cicd test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins pipeline 2
+          Learn Jenkins pipeline 3
         </a>
       </header>
     </div>
